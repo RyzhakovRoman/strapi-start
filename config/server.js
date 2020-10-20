4026829884bcbb515523a312ab25819a0c9ae9af
Roman Ryzhakov <ryzhakov.r@mail.ru>
2020-10-20 06:45:42 +0300

    first-commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec671f05297b6e051acff69142d8e3f7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cda88a77-a417-41c4-a31f-86c9a81acef4'
+};
